7816e617f2f4eb4fbf0495a139844c68599a95fd
muykol <olumuyiwa.kolayemi@slalom.com>
2022-03-12 19:46:58 -0500

    created a .NET web app and edit the index page

---
+++ Muyi-Net-App/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Muyi_Net_App.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

d1291a0e0aafab8b18d47125c3db6d5105c38489
muykol <olumuyiwa.kolayemi@slalom.com>
2022-03-31 10:31:24 -0400

    updated serverless template file

---
+++ Muyi-Net-App/LambdaEntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+
+namespace Muyi_Net_App
+{
+    public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+    {
+        protected override void Init(IWebHostBuilder builder)
+        {
+            builder
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup()
+                .UseApiGateway();
+        }
+    }
+}
